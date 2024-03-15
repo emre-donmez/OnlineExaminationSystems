6ee4a3578a539534221866dc36f2b8fd02dd2c8c
Emrecan Dönmez <edonmez8@ford.com.tr>
2024-03-15 14:51:58 +0300

    Database was connected. Sample entites which user was created. Simple get request was written for testing database connection.

---
+++ OnlineExaminationSystems.API/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineExaminationSystems.API.Model.Context;
+using OnlineExaminationSystems.API.Model.Entities;
+
+namespace OnlineExaminationSystems.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DapperContext _context;
+
+        public UserController(DapperContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var query = "SELECT * FROM Users";
+            using (var connection = _context.CreateConnection())
+            {
+                var companies = await connection.QueryAsync<User>(query);
+                return Ok(companies.ToList());
+            }
+        }
+    }
+}
---
+++ OnlineExaminationSystems.API/Model/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineExaminationSystems.API.Model.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+        public bool IsDel { get; set; }
+    }
+}
