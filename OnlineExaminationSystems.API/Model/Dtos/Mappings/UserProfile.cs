27d0e6116a81f88bc9c47b602b7cdf687b8f88c4
Emrecan <emrecandonmez.bt@gmail.com>
2024-03-17 15:44:30 +0300

    Generic repository was created. And arranged according to user controller repository.

---
+++ OnlineExaminationSystems.API/Model/Dtos/Mappings/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using OnlineExaminationSystems.API.Model.Dtos.User;
+
+namespace OnlineExaminationSystems.API.Model.Dtos.Mappings
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            this.CreateMap<Entities.User, UserUpdateRequestModel>().ReverseMap();
+        }
+    }
+}
--- OnlineExaminationSystems.API/Model/Entities/User.cs
+++ OnlineExaminationSystems.API/Model/Entities/User.cs
@@ -1,6 +1,6 @@
 ﻿namespace OnlineExaminationSystems.API.Model.Entities
 {
-    public class User
+    public class User : IEntity
     {
         public int Id { get; set; }
         public string Name { get; set; }
