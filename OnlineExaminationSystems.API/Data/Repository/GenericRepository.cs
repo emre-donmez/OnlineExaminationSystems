4cb47774bf3f0e3719f98ac181513d64b74f8946
Emrecan <emrecandonmez.bt@gmail.com>
2024-05-04 03:46:57 +0300

    Refactoring get columnames in generic repository.

--- OnlineExaminationSystems.API/Data/Repository/GenericRepository.cs
+++ OnlineExaminationSystems.API/Data/Repository/GenericRepository.cs
@@ -1,7 +1,6 @@
 ﻿using Dapper;
 using OnlineExaminationSystems.API.Data.Context;
 using OnlineExaminationSystems.API.Model.Entities;
-using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Reflection;
 
@@ -11,17 +10,58 @@ namespace OnlineExaminationSystems.API.Model.Repository
     {
         private readonly DapperContext _context;
         private readonly string _tableName;
+        private readonly List<string> _columnNamesWithAttribute;
         private readonly List<string> _columnNames;
-        private readonly List<string> _columnNamesForValue;
         private readonly List<string> _columnNamesForSelection;
+        private readonly List<string> _columnNamesForUpdate;
 
         public GenericRepository(DapperContext context)
         {
             _context = context;
             _tableName = GetTableName();
-            _columnNames = GetColumnNames(false);
-            _columnNamesForValue = GetColumnNamesForValue();
-            _columnNamesForSelection = GetColumnNames(true);
+
+            var (columnNamesWithAttribute, columnNames, columnNamesForSelection, columnNamesForUpdate) = GetColumnNamesAll();
+
+            _columnNamesWithAttribute = columnNamesWithAttribute;
+            _columnNames = columnNames;
+            _columnNamesForSelection = columnNamesForSelection;
+            _columnNamesForUpdate = columnNamesForUpdate;
+        }
+
+        private (List<string> columnNamesWithAttribute, List<string> columnNames, List<string> selectionColumns,List<string> columnNamesForUpdate) GetColumnNamesAll()
+        {
+            var columnNamesWithAttribute = new List<string>();
+            var columnNames = new List<string>();
+            var selectionColumns = new List<string>();
+            var columnNamesForUpdate = new List<string>();
+
+            foreach (var property in typeof(T).GetProperties())
+            {
+                var columnName = property.Name;
+                var columnAttribute = property.GetCustomAttribute<ColumnAttribute>();
+              
+                var selection = columnAttribute != null ? $"{columnAttribute.Name} AS {columnName}" : columnName;
+
+                selectionColumns.Add(selection);
+
+                if (property.Name == "Id") continue;
+
+                columnNames.Add(columnName);
+
+                string value = $"{columnName} =@{columnName}";
+                string column = columnName;
+
+                if (columnAttribute != null)
+                {
+                    value = $"{columnAttribute.Name} =@{columnName}";
+                    column = columnAttribute.Name;
+                }
+
+                columnNamesWithAttribute.Add(column);
+                columnNamesForUpdate.Add(value);
+            }
+
+            return (columnNamesWithAttribute, columnNames, selectionColumns, columnNamesForUpdate);
         }
 
         private static string GetTableName()
@@ -32,59 +72,9 @@ namespace OnlineExaminationSystems.API.Model.Repository
             return tableAttribute == null ? type.Name : tableAttribute.Name;
         }
 
-        private static List<string> GetColumnNamesForValue()
-        {
-            return typeof(T).GetProperties().Where(p => p.Name != "Id").Select(p => p.Name).ToList();
-        }
-
-        private static List<string> GetColumnNames(bool includeAlias = false)
-        {
-            var columnNames = new List<string>();
-
-            foreach (var property in typeof(T).GetProperties())
-            {
-                if (property.Name == "Id" && !includeAlias) continue;
-
-                var columnName = property.Name.ToLowerInvariant();
-                var columnAttribute = property.GetCustomAttribute<ColumnAttribute>();
-
-                if (columnAttribute != null && includeAlias)
-                    columnName = $"{columnAttribute.Name} As {columnName}";
-                else if (columnAttribute != null)
-                    columnName = columnAttribute.Name;
-
-                columnNames.Add(columnName);
-            }
-
-            return columnNames;
-        }
-
-        private IEnumerable<string> SetValues()
-        {
-            var columnNames = new List<string>();
-
-            foreach (var property in typeof(T).GetProperties())
-            {
-                if (property.Name == "Id") continue;
-
-                var columnName = property.Name;
-                var columnAttribute = property.GetCustomAttribute<ColumnAttribute>();
-
-                if (columnAttribute != null)
-                    columnName = $"{columnAttribute.Name} =@{columnName}";
-                else
-                    columnName = $"{columnName} =@{columnName}";
-
-                columnNames.Add(columnName);
-            }
-
-            return columnNames;
-
-        }
-
         public T Insert(T model)
         {
-            var query = $"INSERT INTO {_tableName} ({string.Join(',', _columnNames)}) VALUES (@{string.Join(", @", _columnNamesForValue)})" +
+            var query = $"INSERT INTO {_tableName} ({string.Join(',', _columnNamesWithAttribute)}) VALUES (@{string.Join(", @", _columnNames)})" +
                         "SELECT CAST(SCOPE_IDENTITY() as int)";
 
             using (var connection = _context.CreateConnection())
@@ -134,9 +124,7 @@ namespace OnlineExaminationSystems.API.Model.Repository
 
         public T Update(T model)
         {
-            IEnumerable<string> setValues = SetValues();
-
-            var query = $"UPDATE {_tableName} SET {string.Join(", ", setValues)} WHERE id = @Id AND is_del = 0";
+            var query = $"UPDATE {_tableName} SET {string.Join(", ", _columnNamesForUpdate)} WHERE id = @Id AND is_del = 0";
 
             using (var connection = _context.CreateConnection())
             {
