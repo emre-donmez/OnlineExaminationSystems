6babb7ee3a12f1a82b0a4d54fa491e3a3e0950ab
Emrecan <emrecandonmez.bt@gmail.com>
2024-05-11 00:44:54 +0300

    Question endpoints were done.

---
+++ OnlineExaminationSystems.API/Models/Dtos/QuestionGetResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineExaminationSystems.API.Models.Dtos
+{
+    public class QuestionGetResponseModel
+    {
+        public int Id { get; set; }
+        public string QuestionText { get; set; }
+        public int ExamId { get; set; }
+        public List<string> Options { get; set; }
+    }
+}
---
+++ OnlineExaminationSystems.API/Services/Concrete/QuestionsService.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using OnlineExaminationSystems.API.Data.Repository;
+using OnlineExaminationSystems.API.Models.Dtos;
+using OnlineExaminationSystems.API.Models.Entities;
+using OnlineExaminationSystems.API.Services.Abstract;
+
+namespace OnlineExaminationSystems.API.Services.Concrete
+{
+    public class QuestionsService : CrudService<Question>, IQuestionsService
+    {
+        public QuestionsService(IGenericRepository<Question> repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+
+        public new IEnumerable<QuestionGetResponseModel> GetAll() 
+        {
+            var questions = _repository.GetAll();
+            return _mapper.Map<IEnumerable<QuestionGetResponseModel>>(questions);
+        }
+
+        public new QuestionGetResponseModel GetById(int id)
+        {
+            var question = _repository.GetById(id);
+            return _mapper.Map<QuestionGetResponseModel>(question);
+        }
+    }
+}
