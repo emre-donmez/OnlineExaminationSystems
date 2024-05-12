7fea1d6a115e970a0ae06f27602c79fac35307b2
Emrecan <emrecandonmez.bt@gmail.com>
2024-05-12 17:00:51 +0300

    Results endpoints were done.

---
+++ OnlineExaminationSystems.API/Services/Concrete/ResultsService.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using OnlineExaminationSystems.API.Data.Repository;
+using OnlineExaminationSystems.API.Models.Entities;
+using OnlineExaminationSystems.API.Services.Abstract;
+
+namespace OnlineExaminationSystems.API.Services.Concrete
+{
+    public class ResultsService : CrudService<Result>, IResultsService
+    {
+        public ResultsService(IGenericRepository<Result> repository, IMapper mapper) : base(repository, mapper)
+        {
+        }
+    }
+}
