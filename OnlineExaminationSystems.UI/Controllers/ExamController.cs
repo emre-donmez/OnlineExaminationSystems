7b08e4c5a62e99c324869476f5840a6a84982f6c
Çağatay YEŞİLYURT <cagatayyesilyurt@ogr.eskisehir.edu.tr>
2024-05-15 00:32:00 +0300

    Exam controller was created.

--- OnlineExaminationSystems.UI/Controllers/ExamController.cs
+++ OnlineExaminationSystems.UI/Controllers/ExamController.cs
@@ -1,12 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineExaminationSystems.UI.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace OnlineExaminationSystems.UI.Controllers
 {
     public class ExamController : Controller
     {
-        public IActionResult ExamPage()
+        
+       
+        public async Task<IActionResult> Exams()
         {
-            return View();
+            IEnumerable<Exam> exams = null;
+            using(var client=new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:7100/api/");
+                //HTTP GET
+                var result = await client.GetAsync("Exams");
+         
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = await result.Content.ReadAsStringAsync();
+                    var sd = JsonSerializer.Deserialize<List<Exam>>(readTask);
+                }
+                else
+                {
+                    exams = Enumerable.Empty<Exam>();
+                    ModelState.AddModelError(string.Empty, "Server Error.");
+                }
+
+            }
+            return View(exams);
         }
     }
 }
