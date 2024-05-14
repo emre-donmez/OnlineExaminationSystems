08ab8ab6653722de2c188389f4149a562a66fb9c
cagatayesilyurt <cagatayesilyurt@gmail.com>
2024-05-14 12:01:36 +0300

    Some html files created

---
+++ OnlineExaminationSystems.UI/Controllers/Academician.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineExaminationSystems.UI.Controllers
+{
+    public class Academician : Controller
+    {
+        public IActionResult CreateQuestion()
+        {
+            return View();
+        }
+        public IActionResult Questions() {
+
+            return View();
+        }
+    }
+}
---
+++ OnlineExaminationSystems.UI/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineExaminationSystems.UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        
+    }
+}
