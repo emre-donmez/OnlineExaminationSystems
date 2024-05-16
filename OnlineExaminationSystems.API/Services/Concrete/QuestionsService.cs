711b52eaec4714ce4fa8bb3afed538bf40df68c0
Emrecan <emrecandonmez.bt@gmail.com>
2024-05-16 21:44:25 +0300

    GetQuestionsByExamIdForExam bugs that while getting there is no exam id was solved.

--- OnlineExaminationSystems.API/Services/Concrete/QuestionsService.cs
+++ OnlineExaminationSystems.API/Services/Concrete/QuestionsService.cs
@@ -24,7 +24,7 @@ namespace OnlineExaminationSystems.API.Services.Concrete
 
         public IEnumerable<QuestionGetResponseModel> GetQuestionsByExamIdForExam(int examId)
         {
-            var query = "SELECT TOP (SELECT question_count FROM exams WHERE id = @ExamId) id AS Id,question_text AS QuestionText,wrong_answer_1 AS Option1,wrong_answer_2 AS Option2,wrong_answer_3 AS Option3,correct_answer AS CorrectAnswer,exam_id AS ExamId FROM Questions where exam_id =@ExamId ORDER BY NEWID()";
+            var query = "SELECT TOP (SELECT CASE WHEN EXISTS (SELECT 1 FROM exams WHERE id = @ExamId) THEN (SELECT question_count FROM exams WHERE id = @ExamId) ELSE 0  END)  id AS Id,question_text AS QuestionText,wrong_answer_1 AS Option1,wrong_answer_2 AS Option2,wrong_answer_3 AS Option3,correct_answer AS CorrectAnswer,exam_id AS ExamId FROM Questions where exam_id =@ExamId ORDER BY NEWID()";
 
             var parameters = new { ExamId = examId };
             var questions = _repository.ExecuteQuery(query, parameters);
