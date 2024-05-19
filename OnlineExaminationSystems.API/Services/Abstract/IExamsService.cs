a625235b915083ea9816d276a4e0971d659ae422
Emrecan <emrecandonmez.bt@gmail.com>
2024-05-19 16:00:54 +0300

    GetExamsByLessonId has been written.

--- OnlineExaminationSystems.API/Services/Abstract/IExamsService.cs
+++ OnlineExaminationSystems.API/Services/Abstract/IExamsService.cs
@@ -4,5 +4,6 @@ namespace OnlineExaminationSystems.API.Services.Abstract
 {
     public interface IExamsService : ICrudService<Exam>
     {
+        IEnumerable<Exam> GetExamsByLessonId(int lessonId);
     }
 }
